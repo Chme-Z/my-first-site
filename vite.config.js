dd9f5b3502514045a6447a171f358e218864d0b2
Chme-Z <chme0613@gmail.com>
2026-04-19 17:30:46 +0800

    add github actions

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/my-first-site/', // ⚠️ 这里必须和仓库名一致
 })
